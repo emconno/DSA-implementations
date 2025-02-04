79cf282dee4c677ce0eef55e495dc1fc4ed895e1
emconno <emconno22@gmail.com>
2025-02-04 12:47:33 -0600

    implement insertion sort and binary search

--- AlgorithmUtils.java
+++ AlgorithmUtils.java
@@ -1,11 +1,32 @@
 public class AlgorithmUtils {
     
-    public static int binarySearch(int[] arr) {
+    public static int binarySearch(int[] arr, int target) {
+        int lo = 0, mid = 0, hi = arr.length;
+        
+        while (lo < hi) {
+            mid = lo + (hi-lo) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
         return -1;
     }
 
     public static void insertionSort(int[] arr) {
-
+        for (int i = 1; i < arr.length; i++) {
+            int j = i;
+            while (j > 0 && arr[j] < arr[j-1]) {
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+                j--;
+            }
+        }
     }
 
 }
