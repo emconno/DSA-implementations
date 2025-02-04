f36c16f5c28c22f1c226f4753f7f9f00a45c79e3
emconno <emconno22@gmail.com>
2025-02-04 11:43:24 -0600

    add classes for algorithms and linked list

---
+++ AlgorithmUtils.java
@@ -0,0 +1,11 @@
+public class AlgorithmUtils {
+    
+    public static int binarySearch(int[] arr) {
+        return -1;
+    }
+
+    public static void insertionSort(int[] arr) {
+
+    }
+
+}
---
+++ LinkedList.java
@@ -0,0 +1,37 @@
+public class LinkedList {
+    public Node head;
+
+    public LinkedList() {
+        this.head = null;
+    }
+
+    public LinkedList(Node node) {
+        this.head = node;
+    }
+
+    
+    public void addToHead() {
+
+    }
+
+    public void addToTail() {
+
+    }
+
+    public void removeHead() {
+
+    }
+
+    public void remove(String data) {
+
+    }
+
+    public void swap(String data1, String data2) {
+
+    }
+
+    public String toString() {
+        return "";
+    }
+    
+}
