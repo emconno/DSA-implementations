5c9d124416abbd851883a03ddb24f0cadce42792
emconno <emconno22@gmail.com>
2025-03-12 00:05:11 -0500

    add nthFibonacci and nFactorial

--- LeetCode.java
+++ LeetCode.java
@@ -48,15 +48,26 @@ public class LeetCode {
         return 0;
     }
 
-    public int nthFibonacci() {
+    public static int nthFibonacci(int n) {
+        //assuming the first number in the sequence is 1 (0 returns 0)
+        if (n < 2) {
+            return n; 
+        }
+
+        return nthFibonacci(n-1) + nthFibonacci(n-2);
 
     }
 
-    public int nthFactorial() {
-        
+    public static int nFactorial(int n) {
+        if (n < 3) {
+            return n;
+        }
+
+        return n * nFactorial(n - 1);
     }
 
     public static void main(String[] args) {
-        
+        System.out.println(nthFibonacci(21));
+        System.out.println(nFactorial(3));
     }
 }
